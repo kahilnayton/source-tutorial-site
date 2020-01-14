c343e3893b155fddea5cd67b38f158a77a6dfb51
kahil nayton <kahilnayton@gmail.com>
2020-01-14 09:04:25 -0500

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,60 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 
 
-console.log(data)
 
-export default ({ data }) => <>
-  <h1>Author</h1>
-  <h1>title</h1>
-  <img>Imige</img>
-  <ul>
-    {data.allNewsArticle.nodes.map(article => (
-      <li key={article.id}>
-        <h2>{article.title}</h2>
-        <p>{article.author}</p>
-        <img src={article.urlToImage} alt="Image url"/>
-      </li>
-    ))}
-  </ul>
-</>
+class IndexPage extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const siteDescription = data.site.siteMetadata.description
+    const photos = data.allPixabayPhoto.largeImageURL
+      
+    return (
+      <div>
+        <h1>Author</h1>
+        <h1>title</h1>
+        {/* <ul>
+          {data.allNewsArticle.node.map(article => (
+            <li key={article.node.id}>
+              <h2>{article.node.title}</h2>
+              <p>{article.node.author}</p>
+              <img src={article.urlToImage} alt="Image url"/>
+            </li>
+          ))}
+        </ul> */}
+      </div>
+    )
+  }
+}
 
-
-export const query = graphql`
- {
+export const pageQuery = graphql`
+{
   allPixabayPhoto {
     edges {
       node {
+        id
         largeImageURL
       }
     }
   }
   allNewsArticle {
     nodes {
+      id
       author
       title
       urlToImage
     }
   }
+  site {
+    siteMetadata {
+      title
+      description
+    }
+  }
 }
 
+
 `
+
+export default IndexPage
